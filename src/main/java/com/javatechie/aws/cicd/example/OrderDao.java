784c916724041e4352aca1d440fd057aba9087e8
Elveena023 <98262749+Elveena023@users.noreply.github.com>
2022-08-03 18:36:07 +0530

    Update OrderDao.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -15,7 +15,7 @@ public class OrderDao {
                 new Order(101, "Mobile", 1, 30000),
                 new Order(58, "Book", 4, 2000),
                 new Order(205, "Laptop", 1, 150000),
-                new Order(809, "headset", 1, 1800))
+                new Order(809, "headset", 1, 1900))
                 .collect(Collectors.toList());
     }
 }
